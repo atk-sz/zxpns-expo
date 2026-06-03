b9f1609f870bc4978860162dd63880d71b72867b
Syed Zaid <syed.zaid525@gmail.com>
2026-06-03 16:41:08 +0530

    removed prescreen & updated dev screen to index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,104 @@
 import ScreenView from "@/components/generic/ScreenView";
-import LoaderComponent from "@/components/loader/loader.component";
+import { theme } from "@/constants/theme";
+import { useLoader } from "@/contexts/loader.context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const InitLoadScreen: React.FC = () => {
+const DevScreen: React.FC = (): React.JSX.Element => {
+  const { showLoader, hideLoader } = useLoader();
+
+  // to acces data from async storage
+  // const loadUser = async () => {
+  //   try {
+  //     const persistedState = await AsyncStorage.getItem('persist:root');
+  //     console.log('persistedState', persistedState);
+  //     if (persistedState) {
+  //       const parsedState = JSON.parse(persistedState);
+  //       console.log('parsedState', parsedState);
+
+  //       // user slice is stringified JSON, so parse it again
+  //       const userState = JSON.parse(parsedState.user);
+  //       console.log('userState', userState);
+  //       // setUser(userState)
+  //     }
+  //   } catch (err) {
+  //     console.error('Error loading user:', err);
+  //   }
+  // };
+
+  const showLoaderFn = (): void => {
+    showLoader("Brrr...");
+    setTimeout(() => {
+      hideLoader();
+    }, 3000);
+  };
+
+  const removeData = async () => {
+    try {
+      await AsyncStorage.clear();
+      // router.replace("/PreScreen");
+    } catch (e) {
+      console.error("Failed to clear storage:", e);
+    }
+  };
+
+  const goToHome = (): void => {
+    router.push("/Home");
+  };
+
+  const goToLogin = (): void => {
+    router.push("/Login");
+  };
+
+  const goToProfile = (): void => {};
+
   return (
     <ScreenView>
-      <LoaderComponent />
+      <Text style={styles.text}>Dev Screen</Text>
+      <View style={styles.container1}>
+        <TouchableOpacity style={styles.btn} onPress={showLoaderFn}>
+          <Text>Show Loading</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={removeData}>
+          <Text>Remove Data</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.btn} onPress={goToHome}>
+        <Text>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={goToProfile}>
+        <Text>Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn} onPress={goToLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </ScreenView>
   );
 };
 
-export default InitLoadScreen;
+export default DevScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: theme.text,
+  },
+  container1: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  btn: {
+    backgroundColor: "#00ff00",
+    margin: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+});
